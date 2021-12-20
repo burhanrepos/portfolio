d85206d53c90732bd2a2d2ece4f6e439347f1424
Burhan <hafiz.burhan834@gmail.com>
2021-12-20 18:33:41 +0500

    Changing in routes

--- src/App.js
+++ src/App.js
@@ -1,45 +1,37 @@
-import './App.css';
-import React from 'react';
-import Services from './pages/Services';
+import "./App.css";
+import React from "react";
+import Services from "./pages/Services";
 import Projects from "./pages/Projects";
-import Contact from './pages/Contact';
-import About from './pages/About';
-// import { Route, Switch } from 'react-router-dom';
-import { BrowserRouter as Router,Routes, Route, Link } from "react-router-dom";
+import Contact from "./pages/Contact";
+import About from "./pages/About";
+import { Route } from "react-router-dom";
+import { Switch } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+// import { BrowserRouter as Router,Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
-  //  <Switch>
-  //     <Route exact path="/">
-  //       <About />
-  //     </Route>
+    <Router>
+      <Switch>
+        <Route exact path="/portfolio" component={About} />
 
+        <Route path="/portfolio/projects" component={Projects} />
 
-  //     <Route path="/projects">
-  //       <Projects /> 
-  //      </Route>
+        <Route path="/portfolio/services" component={Services} />
 
+        <Route path="/portfolio/contact" component={Contact} />
+      </Switch>
+    </Router>
 
-  //     <Route path="/services">
-  //       <Services />
-  //     </Route>
-
-
-  //     <Route path="/contact">
-  //       <Contact />
-  //     </Route>
-
-  //   </Switch>
-  <Router>
-      <Routes>
-        <Route path="/portfolio/contact" caseSensitive={false} element={<Contact />} />
-        <Route path="/portfolio/projects" caseSensitive={false} element={<Projects />} />
-        <Route path="/portfolio/services" caseSensitive={false} element={<Services />} />
-        <Route path="/portfolio" caseSensitive={false} element={<About />} />
-      </Routes>
-    </Router> 
   );
-}
+};
 
 export default App;
-
+    // <Router>
+    //     <Routes>
+    //       <Route path="/portfolio/contact" caseSensitive={false} element={<Contact />} />
+    //       <Route path="/portfolio/projects" caseSensitive={false} element={<Projects />} />
+    //       <Route path="/portfolio/services" caseSensitive={false} element={<Services />} />
+    //       <Route path="/portfolio" caseSensitive={false} element={<About />} />
+    //     </Routes>
+    //   </Router>
